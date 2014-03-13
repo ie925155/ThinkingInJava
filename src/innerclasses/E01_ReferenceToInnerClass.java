f5f2e49b8d48ccdce643c03f5d0bfb6ed87d6dfd
sheldon.lai <ie925155@gmail.com>
2014-03-14 01:40:13 +0800

    - update innerclass exercises

---
+++ src/innerclasses/E01_ReferenceToInnerClass.java
@@ -0,0 +1,23 @@
+package innerclasses;
+
+/****************** Exercise 1 *****************
+* Write a class named Outer containing an
+* inner class named Inner. Add a method to Outer
+* that returns an object of type Inner. In
+* main(), create and initialize a reference to
+* an Inner.
+***********************************************/
+
+class Outer {
+  class Inner {
+	{ System.out.println("Inner created"); }
+  }
+  Inner getInner() { return new Inner(); }
+}
+
+public class E01_ReferenceToInnerClass {
+  public static void main(String[] args) {
+	Outer o = new Outer();
+	Outer.Inner i = o.getInner();
+  }
+}
